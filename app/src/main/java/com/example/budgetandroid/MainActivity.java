0d6f4bdfa8950c2319b997a35b4568fab73d66b7
Przemysław Skolimski <pskolimski@revku.dev>
2025-03-13 10:06:29 +0100

    init

---
+++ app/src/main/java/com/example/budgetandroid/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.budgetandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private boolean isLogged = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        if (isLogged) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        }
+    }
+}
